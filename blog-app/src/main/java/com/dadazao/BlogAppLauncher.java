ecad6197d02e4e124e553893f0ca50c1836657c5
liheng <li15038043160@163.com>
2018-09-13 10:26:39 +0800

    刷新配置中心配置

--- blog-app/src/main/java/com/dadazao/BlogAppLauncher.java
+++ blog-app/src/main/java/com/dadazao/BlogAppLauncher.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
@@ -16,6 +17,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableHystrix
 @EnableHystrixDashboard
 @EnableCircuitBreaker
+@RefreshScope
 public class BlogAppLauncher {
 
     public static void main(String[] args) {
