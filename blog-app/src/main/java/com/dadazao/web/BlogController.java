a7d24dbc6690067dd0ad27afeb84908219186bca
liheng <li15038043160@163.com>
2018-10-29 11:48:47 +0800

    断路器演示

--- blog-app/src/main/java/com/dadazao/web/BlogController.java
+++ blog-app/src/main/java/com/dadazao/web/BlogController.java
@@ -5,6 +5,7 @@ import com.dadazao.api.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -24,6 +25,14 @@ public class BlogController {
         Result result = new Result();
         result.setCode("200");
         result.setResult(blogService.list());
+        return result;
+    }
+
+    @GetMapping("/blog/get/{id}")
+    public Result get(@PathVariable("id") Long id) {
+        Result result = new Result();
+        result.setCode("200");
+        result.setResult(blogService.get(id));
         return result;
     }
 
